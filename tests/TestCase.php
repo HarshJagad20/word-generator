574456f592463c5148ae3e3d03f68dc9f630eda0
HarshJagad20 <HarshJagad20@users.noreply.github.com>
2022-08-12 05:44:57 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace Harsh\WordGenerator\Tests;
 
+use Harsh\WordGenerator\WordGeneratorServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Harsh\WordGenerator\WordGeneratorServiceProvider;
 
 class TestCase extends Orchestra
 {

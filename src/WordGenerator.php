ee32c9eb8af6555649eb6186f5097c745b4fa624
HarshJagad20 <HarshJagad20@users.noreply.github.com>
2022-08-15 08:31:03 +0000

    Fix styling

--- src/WordGenerator.php
+++ src/WordGenerator.php
@@ -12,39 +12,39 @@ class WordGenerator
 
         return [
             'all' => [
-                'nouns'      => Arr::random(config('word-generator.nouns'), $count),
-                'verbs'      => Arr::random(config('word-generator.verbs'), $count),
+                'nouns' => Arr::random(config('word-generator.nouns'), $count),
+                'verbs' => Arr::random(config('word-generator.verbs'), $count),
                 'adjectives' => Arr::random(config('word-generator.adjectives'), $count),
-            ]
+            ],
         ];
     }
 
     public function nouns($count = null)
     {
-        $count    = $count ?? 5;
-        $array    = config('word-generator.nouns');
+        $count = $count ?? 5;
+        $array = config('word-generator.nouns');
         $response = Arr::random($array, $count);
 
-        return ['nouns'=>$response];
+        return ['nouns' => $response];
     }
 
     public function verbs($count = null)
     {
         $count = $count ?? 5;
 
-        $array    = config('word-generator.verbs');
+        $array = config('word-generator.verbs');
         $response = Arr::random($array, $count);
 
-        return ['verbs'=>$response];
+        return ['verbs' => $response];
     }
 
     public function adjectives($count = null)
     {
         $count = $count ?? 5;
 
-        $array    = config('word-generator.adjectives');
+        $array = config('word-generator.adjectives');
         $response = Arr::random($array, $count);
 
-        return ['adjectives'=>$response];
+        return ['adjectives' => $response];
     }
 }
--- tests/WordGeneratorTest.php
+++ tests/WordGeneratorTest.php
@@ -3,30 +3,30 @@
 use Harsh\WordGenerator\WordGenerator;
 
 test('get nouns', function () {
-    $count         = 5;
-    $response      = new WordGenerator();
-    $response      = $response->nouns($count);
+    $count = 5;
+    $response = new WordGenerator();
+    $response = $response->nouns($count);
     $this->assertCount($count, $response['nouns']);
 });
 
 test('get adjectives', function () {
-    $count         = 5;
-    $response      = new WordGenerator();
-    $response      = $response->adjectives($count);
+    $count = 5;
+    $response = new WordGenerator();
+    $response = $response->adjectives($count);
     $this->assertCount($count, $response['adjectives']);
 });
 
 test('get verbs', function () {
-    $count         = 5;
-    $response      = new WordGenerator();
-    $response      = $response->verbs($count);
+    $count = 5;
+    $response = new WordGenerator();
+    $response = $response->verbs($count);
     $this->assertCount($count, $response['verbs']);
 });
 
 test('get all words', function () {
-    $count         = 5;
-    $response      = new WordGenerator();
-    $response      = $response->all($count);
+    $count = 5;
+    $response = new WordGenerator();
+    $response = $response->all($count);
 
     foreach ($response['all'] as $value) {
         $this->assertCount($count, $value);
